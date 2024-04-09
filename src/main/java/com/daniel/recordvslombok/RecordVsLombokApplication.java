9768552e7429d5ebfbd7c1e6de21b60ce9793a8b
Daniel Penelva <d4n.andrade@gmail.com>
2024-04-09 04:07:44 -0300

    criando projeto spring boot records VS lombok

---
+++ src/main/java/com/daniel/recordvslombok/RecordVsLombokApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.recordvslombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordVsLombokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordVsLombokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daniel/recordvslombok/RecordVsLombokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.recordvslombok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordVsLombokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
